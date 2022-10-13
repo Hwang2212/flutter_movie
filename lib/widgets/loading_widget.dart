f0fd6043b08a1889c60725f9214c936e85c1c487
Hwang2212 <andyhwangsauyee@gmail.com>
2022-10-13 18:44:00 +0800

    lkala

---
+++ lib/widgets/loading_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter_movie/utils/widget_helpers.dart' as widgetHelper;
+
+class LoadingIndicator extends StatelessWidget {
+  const LoadingIndicator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: widgetHelper.showLoadingIndicator(),
+      ),
+    );
+  }
+}
