69a699fd63042652617b86d9319b9cc5bd05aa3f
Andy Artisan <andy@artisan.com.my>
2022-11-08 10:19:14 +0800

    Color Manager

---
+++ lib/presentation/src/color_manager.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const Color primary = Color(0xFF6C4AB6);
+  static const Color primary = Color(0xFF8D72E1);
+  static const Color primary = Color(0xFF8D9EFF);
+  static const Color primary = Color(0xFFB9E0FF);
+}
